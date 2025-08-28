7242d78deda8a32a32416a91bc218aa40fa75a14
Your Name <you@example.com>
2025-08-28 20:01:38 +0300

    домашка

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,104 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //#1
+        int clientOS = 1; //0=ios. 1=Android
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    //#2
+    int clientOs2 = 1;
+    int clientDeviceYear = 2015;
+        if(clientOs2 == 0)
+
+    {
+        if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    } else if(clientOs2 ==1)
+
+    {
+        if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
+    //#3
+    int year = 2023;
+        if((year %4==0&&year %100!=0)||year %400==0)
+
+    {
+        System.out.println(year + " год является високосным");
+    } else
+
+    {
+        System.out.println(year + " год не является високосным");
+    }
+
+    //#4
+    int deliveryDistance = 61;
+    int dayDelivery = 1;
+    int dayDelivery1 = dayDelivery + 1;
+    int dayDelivery2 = dayDelivery + 2;
+        if(deliveryDistance <=20)
+
+    {
+        System.out.println("Потребуется дней: " + dayDelivery);
+    }
+        if(deliveryDistance >20&&deliveryDistance <=60)
+
+    {
+        System.out.println("Потребуется дней: " + dayDelivery1);
+    }
+        if(deliveryDistance >60&&deliveryDistance <=100)
+
+    {
+        System.out.println("Потребуется дней: " + dayDelivery2);
+    }
+        if(deliveryDistance >100)
+
+    {
+        System.out.println("доставки нет");
+    }
+
+    //#5
+    int monthNumber = 12;
+        switch(monthNumber)
+
+    {
+        case 12:
+        case 1:
+        case 2:
+            System.out.println("Сейчас зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("Сейчас весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            System.out.println("Сейчас лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            System.out.println("Сейчас осень");
+            break;
+        default:
+            System.out.println("Такого месяца не существует");
+    }
+
+
+}
 }
